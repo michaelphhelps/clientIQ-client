90a10683635faddee93be640864e7d3ae6e285ef
Michael Phillips <mphillips242001@outlook.com>
2025-10-26 20:44:59 -0500

    messing around

--- src/App.jsx
+++ src/App.jsx
@@ -32,9 +32,9 @@ function App() {
             <Route
               path="/dashboard"
               element={
-                <ProtectedRoute>
-                  <Dashboard />
-                </ProtectedRoute>
+                //<ProtectedRoute>
+                <Dashboard />
+                // </ProtectedRoute>
               }
             />
             <Route
